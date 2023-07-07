ba29fde711551ce079b791db2628f51732343db1
ananmhrd <127778562+ananmhrd@users.noreply.github.com>
2023-07-08 00:20:21 +0700

    Update add_metadata.py

--- Coursera/Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning/Modul 2/Implementing Callbacks in TensorFlow using the MNIST Dataset/add_metadata.py
+++ Coursera/Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning/Modul 2/Implementing Callbacks in TensorFlow using the MNIST Dataset/add_metadata.py
@@ -28,7 +28,7 @@ parser.add_argument('--filename',
                     help='filename of the notebook to check')
 args = parser.parse_args()
 
-ntbk = nbf.read(f'./{args.filename}', nbf.NO_CONVERT)
+ntbk = nbf.read(f'/content/machine_learning/Coursera/Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning/Modul 2/Implementing Callbacks in TensorFlow using the MNIST Dataset/{args.filename}', nbf.NO_CONVERT)
 
 for cell in ntbk.cells:
     
